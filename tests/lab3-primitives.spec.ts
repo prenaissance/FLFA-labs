e4da6b759217f5f97b2b8171d2877768155a69ff
prenaissance <alexandru.andries@isa.utm.md>
2023-03-21 16:22:01 +0200

    test: setup tests for lab 3

---
+++ tests/lab3-primitives.spec.ts
@@ -0,0 +1,33 @@
+import { describe, it, expect } from "vitest";
+import * as E from "fp-ts/Either";
+import * as P from "@/parser";
+
+describe("lab3 -> primitives", () => {
+  it("should parser single characters", () => {
+    const parser = P.char("a");
+    const result = P.run("abc")(parser);
+    expect(result._tag === "Right" && result.right.value).toBe("a");
+    expect(result._tag === "Right" && result.right.nextInput).toEqual({
+      text: "abc",
+      index: 1,
+    });
+  });
+
+  it("should fail on unexpected characters", () => {
+    const parser = P.char("a");
+    const result = P.run("bcd")(parser);
+    expect(result._tag === "Left" && result.left.input).toEqual({
+      text: "bcd",
+      index: 0,
+    });
+  });
+
+  it("should fail on end of input", () => {
+    const parser = P.char("a");
+    const result = P.run("")(parser);
+    expect(result._tag === "Left" && result.left.input).toEqual({
+      text: "",
+      index: 0,
+    });
+  });
+});
