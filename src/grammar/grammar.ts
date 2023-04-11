001f8f2f2bb8539db80dba4147a21d7d80f4e88b
prenaissance <alexandru.andries@isa.utm.md>
2023-04-11 23:29:02 +0300

    fix(lab4): fixed null production filtering case

--- src/grammar/grammar.ts
+++ src/grammar/grammar.ts
@@ -110,17 +110,36 @@ export class Grammar {
     if (nullProduction === undefined) {
       return this.clone();
     }
+    const [nullProductionSymbol] = nullProduction.from;
+    const nonTerminalProductions = productions.filter(
+      (p) => p !== nullProduction,
+    );
+    const hasAlternateProductions = nonTerminalProductions.some(
+      ({ from }) => from[0] === nullProductionSymbol,
+    );
+
+    if (!hasAlternateProductions) {
+      const newProductions = nonTerminalProductions.map(({ from, to }) => ({
+        from,
+        to: to.filter((word) => word !== nullProductionSymbol),
+      }));
+
+      return new Grammar(
+        start,
+        newProductions,
+        nonTerminal.filter((n) => n !== nullProductionSymbol),
+        terminal,
+      ).withoutNullProductions();
+    }
     // context free grammars (CNF reducible) won't have >= 1 left side symbols
     // so I'll only replace the first symbol in the right side for one null production
     // and recurse
     const combinationsWithoutNullElement = combinationsWithout(
       nullProduction.from[0],
     );
-    const newProductions = productions
-      .filter((p) => p !== nullProduction)
-      .flatMap(({ from, to }) =>
-        combinationsWithoutNullElement(to).map((to) => ({ from, to })),
-      );
+    const newProductions = nonTerminalProductions.flatMap(({ from, to }) =>
+      combinationsWithoutNullElement(to).map((to) => ({ from, to })),
+    );
 
     return new Grammar(
       start,
--- tests/lab4.spec.ts
+++ tests/lab4.spec.ts
@@ -59,6 +59,107 @@ describe("lab4", () => {
 
   it("should remove null productions", () => {
     const grammar = v2Grammar.withoutNullProductions();
-    console.log(JSON.stringify(grammar, null, 2));
+    const expected = {
+      start: "S",
+      productions: [
+        {
+          from: ["S"],
+          to: ["a", "B"],
+        },
+        {
+          from: ["S"],
+          to: ["b"],
+        },
+        {
+          from: ["S"],
+          to: ["b", "A"],
+        },
+        {
+          from: ["A"],
+          to: ["B"],
+        },
+        {
+          from: ["A"],
+          to: ["b"],
+        },
+        {
+          from: ["A"],
+          to: ["a", "D"],
+        },
+        {
+          from: ["A"],
+          to: ["S"],
+        },
+        {
+          from: ["A"],
+          to: ["A", "S"],
+        },
+        {
+          from: ["A"],
+          to: ["b", "B"],
+        },
+        {
+          from: ["A"],
+          to: ["b", "A", "B"],
+        },
+        {
+          from: ["A"],
+          to: ["b", "A", "A", "B"],
+        },
+        {
+          from: ["B"],
+          to: ["b"],
+        },
+        {
+          from: ["B"],
+          to: ["b", "S"],
+        },
+        {
+          from: ["C"],
+          to: ["B"],
+        },
+        {
+          from: ["C"],
+          to: ["A", "B"],
+        },
+        {
+          from: ["D"],
+          to: ["B", "B"],
+        },
+      ],
+      nonTerminal: ["S", "A", "B", "C", "D"],
+      terminal: ["a", "b"],
+    };
+
+    expect(grammar).toEqual(expected);
+  });
+
+  it("should not give combinations when null production doesn't have different outcome", () => {
+    const grammar = new Grammar(
+      "S",
+      [
+        { from: ["S"], to: ["a", "B"] },
+        { from: ["B"], to: ["b"] },
+        { from: ["S"], to: ["b", "A"] },
+        { from: ["A"], to: [] },
+      ],
+      ["S", "A", "B"],
+      ["a", "b"],
+    );
+
+    const grammarWithoutNullProductions = grammar.withoutNullProductions();
+
+    const expected = new Grammar(
+      "S",
+      [
+        { from: ["S"], to: ["a", "B"] },
+        { from: ["B"], to: ["b"] },
+        { from: ["S"], to: ["b"] },
+      ],
+      ["S", "B"],
+      ["a", "b"],
+    );
+
+    expect(grammarWithoutNullProductions).toEqual(expected);
   });
 });
